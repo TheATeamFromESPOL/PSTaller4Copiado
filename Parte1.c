a9542bf2db97bc8a756b117d6b8eb67a24ce0b71
llullanqui <luis.1142@hotmail.com>
2017-05-26 17:17:30 -0500

    Cambio de letras

--- Parte1.c
+++ Parte1.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-#define TLINEA 1000
+#include <string.h>
+#define TLINEA 500
 
+char cifrado[TLINEA];
+int indiceDeCaracter(char caracter);
+int cifrarFrase(char frase[], int espaciado);
 char alfabeto[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
 
 int main(void){
@@ -11,8 +15,33 @@ int main(void){
 	printf("Ingrese la cantidad de espacios: ");
 	scanf("%d",&num);
 	printf("%s %d\n",frase,num);
-	/*for(int i=0;i<sizeof(alfabeto);i++){
-		printf("%c",alfabeto[i]);
-		printf("\n");
-	}*/
+	int longitud = strlen(frase);
+	printf("%s %d\n","Longitud de frase",longitud);
+	printf("\n");
+	cifrarFrase(frase,num);
+	printf("Frase cifrada: ");
+	printf("%s\n",cifrado);
+}
+
+int cifrarFrase(char frase[], int espaciado){
+	int mov;
+	for(int i=0;i<strlen(frase);i++){
+		mov = indiceDeCaracter(frase[i]) + espaciado;
+		if(mov>=sizeof(alfabeto)){
+			mov = mov%26;
+		}
+		cifrado[i] = alfabeto[mov];
+	}
+	return 0;
+}
+
+int indiceDeCaracter(char caracter){
+	int pos;
+	for(int i=0;i<sizeof(alfabeto);i++){
+		if(alfabeto[i]==caracter){
+			pos=i;
+		}
+	}
+	printf("%d",pos);
+	return pos;
 }
