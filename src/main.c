5a75a21703ce88e04d66032f35c99442e9968a68
liarias <lucio_arias96@hotmail.com>
2017-06-04 20:13:27 -0500

    modificaciones

--- src/main.c
+++ src/main.c
@@ -11,6 +11,32 @@ int main(int argc, char *argv[0]){
                 scanf("%[^\n]s",tipoCifrado);
 		printf("Ingrese la frase a cifrar: ");
 		scanf("%[^\n]s",frase);
+		char* fraseCifrada;
+		if(tipoCifrado=='C' || tipoCifrado=='c'||tipoCifrado=="-c"){
+			fraseCifrada = cifradoCiclico(frase,num);
+			printf("Cifrado ciclico: %s\n",fraseCifrada);
+			char* morse = claveMorse(frase);
+			printf("Morse: ");
+			printf("%s\n",morse);
+		}
+		else if(tipoCifrado=="A"||tipoCifrado=="a"||tipoCifrado=="-a"){
+			char* contra;
+			printf("Ingrese la contrasena: ");
+		        scanf("%[^\n]s",contra);
+			fraseCifrada = cifradoAutollave(frase,contra);
+			printf("Cifrado Autollave: %s\n",fraseCifrada");
+		}
+		else if(tipoCifrado=="P"||tipoCifrado=="p"||tipoCifrado=="-p"){
+			char* contra;
+		        printf("Ingrese la contrasena: ");
+		        scanf("%[^\n]s",contra);
+		        fraseCifrada = cifradoContrasena(frase,contra);
+		        printf("Cifrado Contrasena: %s\n",fraseCifrada");
+		}
+		else{
+			printf("\nEse tipo de cifrado no esta disponible");
+			fraseCifrada = "";
+		}
 	}
 	else if(argc==4){
 		int num = atoi(argv[1]);
@@ -20,31 +46,5 @@ int main(int argc, char *argv[0]){
 	else{
 		printf("Debe colocar la correcta cantidad de argumentos\n");
 	}
-	char* fraseCifrada;
-	if(tipoCifrado=="C"||tipoCifrado=="c"||tipoCifrado=="-c"){
-		fraseCifrada = cifradoCiclico(frase,num);
-		printf("Cifrado ciclico: ");
-	}
-/*	else if(tipoCifrado=="A"||tipoCifrado=="a"||tipoCifrado=="-a"){
-		char* contra;
-		printf("Ingrese la contrasena: ");
-                scanf("%[^\n]s",contra);
-		fraseCifrada = cifradoAutollave(frase,contra);
-		printf("Cifrado Autollave: ");
-	}
-	else if(tipoCifrado=="P"||tipoCifrado=="p"||tipoCifrado=="-p"){
-		char* contra;
-                printf("Ingrese la contrasena: ");
-                scanf("%[^\n]s",contra);
-                fraseCifrada = cifradoContrasena(frase,contra);
-                printf("Cifrado Contrasena: ");
-	}
-*/	else{
-		printf("\nEse tipo de cifrado no esta disponible");
-		fraseCifrada = "";
-	}
-	char* morse = claveMorse(frase);
-	printf("%s\n",fraseCifrada);
-	printf("Morse: ");
-	printf("%s\n",morse);
+
 }
